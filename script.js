c59de904084da52016dc78b528300c80d5ca6e61
snayder <snayderleomadrid97@gmail.com>
2023-01-08 21:04:51 -0500

    agregando el archivo javascript para la funcion encriptar y desencriptar

---
+++ script.js
@@ -0,0 +1,45 @@
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+function encriptar() {
+    var texto = document.getElementById("cuadrotexto").value.toLowerCase();
+   
+    var txtcifrado = texto.replace(/e/igm,"enter");
+    var txtcifrado = txtcifrado.replace(/i/igm,"imes");
+    var txtcifrado = txtcifrado.replace(/a/igm,"ai");
+    var txtcifrado = txtcifrado.replace(/o/igm,"ober");
+    var txtcifrado = txtcifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imgmuñeco").style.display = "none";
+    document.getElementById("mnsjsresultado").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtcifrado;
+    document.getElementById("btncopiar").style.display = "show";
+    document.getElementById("btncopiar").style.display = "inherit";
+    document.getElementById("texto2").style.display = "show";
+    document.getElementById("texto2").style.display = "inherit";
+}
+function desencriptar(){
+    var texto = document.getElementById("cuadrotexto").value.toLowerCase();
+   
+   
+    var txtcifrado = texto.replace(/enter/igm,"e");
+    var txtcifrado = txtcifrado.replace(/imes/igm,"i");
+    var txtcifrado = txtcifrado.replace(/ai/igm,"a");
+    var txtcifrado = txtcifrado.replace(/ober/igm,"o");
+    var txtcifrado = txtcifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgmuñeco").style.display = "none";
+    document.getElementById("mnsjsresultado").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtcifrado;
+    document.getElementById("btncopiar").style.display = "show";
+    document.getElementById("btncopiar").style.display = "inherit";
+    document.getElementById("texto2").style.display = "show";
+    document.getElementById("texto2").style.display = "inherit";
+}
+function copiar () {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+}
